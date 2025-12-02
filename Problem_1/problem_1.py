ac36e12564f943f00004c62f02212f345201711f
Blackeye Prime <alexandermeves01@gmail.com>
2025-12-02 17:32:05 -0500

    Problem 1 part 2

--- Problem_1/problem_1.py
+++ Problem_1/problem_1.py
@@ -1,22 +1,45 @@
-## Part 1
+## Part 2
 
 def lockpicking(line, position):
-    #Turn to smaller numbers if left (overflow is modulo-ed)
-    if line[0] == "L":
-        position = (100 + (position - int(line[1::]))) % 100
-    # Turn to higher numbers if right (overflow is modulo-ed)
-    if line[0] == "R":
-        position = (100 + (position + int(line[1::]))) % 100
-    return position
+    line = line.strip()
+    direction = line[0]
+    n = int(line[1:])
+    zero_point = 0
+
+    if direction == "L":
+        if position == 0:
+            zero_point += n // 100
+        else:
+            if n < position:
+                zero_point += 0
+            else:
+                zero_point += 1 + (n - position) // 100
+        position = (position - n) % 100
+
+    elif direction == "R":
+        if position == 0:
+            zero_point += n // 100
+        else:
+            dist_to_zero = 100 - position
+            if n < dist_to_zero:
+                zero_point += 0
+            else:
+                zero_point += 1 + (n - dist_to_zero) // 100
+        position = (position + n) % 100
+
+    return position, zero_point
 
 def readinstructions():
     position = 50
     zero_count = 0
     with open("AoC2025_1.txt") as f:
+        line_num = 1
         for line in f.readlines():
-            position = lockpicking(line, position)
-            if position == 0:
-                zero_count += 1
-    print(zero_count)
+
+            position, zero_point = lockpicking(line, position)
+            print("Line Number: " + str(line_num) + " Position: " + str(position), "Zero Point: " + str(zero_point))
+            zero_count += zero_point
+            print(zero_count)
+            line_num += 1
 
 readinstructions()
